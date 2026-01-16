c4429ce4627c3d4a366143701db3d5d626a823d5
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2026-01-16 19:20:34 +0100

    Add run_all script

---
+++ scripts/run_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+#TODO: add Ant experiment command
+
+pdm run scripts/compare_hyperparameter.py --env h1-walk-v0 --optimize theta --num-cpu 4 --max-processes 2
+pdm run scripts/compare_hyperparameter.py --env h1-walk-v0 --optimize frac --num-cpu 4 --max-processes 2 \
+    --set-theta 0.8
+pdm run scripts/compare_hyperparameter.py --env h1-walk-v0 --optimize time_steps --num-cpu 8 --max-processes 1 \
+    --set-theta 0.8 --set-frac 0.2
+pdm run scripts/compare_hyperparameter.py --env h1-walk-v0 --optimize K --num-cpu 8 --max-processes 1 \
+    --set-theta 0.8 --set-frac 0.2 --set-time-steps 50
+pdm run scripts/compare_hyperparameter.py --env h1-walk-v0 --optimize seed --num-cpu 4 --max-processes 2 \
+    --set-theta 0.8 --set-frac 0.2 --set-time-steps 50 --set-k 1
