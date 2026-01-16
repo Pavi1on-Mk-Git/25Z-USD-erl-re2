265a8f002f5f8793387584d3c899810edc23aeec
Paweł Kochański <pawilioxion@gmail.com>
2026-01-16 21:28:48 +0100

    Add h1-hurdle-v0 experiment to scripts used for running and drawing all experiments

--- scripts/draw_all.sh
+++ scripts/draw_all.sh
@@ -9,3 +9,8 @@ pdm run scripts/draw_plot.py --env h1-walk-v0 --optimize theta --output-file plo
 pdm run scripts/draw_plot.py --env h1-walk-v0 --optimize frac --set-theta 0.8 --output-file plots/walk_frac.svg
 pdm run scripts/draw_plot.py --env h1-walk-v0 --optimize time_steps --set-theta 0.8 --set-frac 0.2 --output-file plots/walk_time_steps.svg
 pdm run scripts/draw_plot.py --env h1-walk-v0 --optimize K --set-theta 0.8 --set-frac 0.2 --set-time-steps 50 --output-file plots/walk_k.svg
+
+pdm run scripts/draw_plot.py --env h1-hurdle-v0 --optimize theta --output-file plots/hurdle_theta.svg
+pdm run scripts/draw_plot.py --env h1-hurdle-v0 --optimize frac --set-theta 0.7 --output-file plots/hurdle_frac.svg
+pdm run scripts/draw_plot.py --env h1-hurdle-v0 --optimize time_steps --set-theta 0.7 --set-frac 0.2 --output-file plots/hurdle_time_steps.svg
+pdm run scripts/draw_plot.py --env h1-hurdle-v0 --optimize K --set-theta 0.7 --set-frac 0.2 --set-time-steps 50 --output-file plots/hurdle_k.svg
--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -12,3 +12,13 @@ pdm run scripts/compare_hyperparameter.py --env h1-walk-v0 --optimize K --num-cp
     --set-theta 0.8 --set-frac 0.2 --set-time-steps 50
 pdm run scripts/compare_hyperparameter.py --env h1-walk-v0 --optimize seed --num-cpu 4 --max-processes 2 \
     --set-theta 0.8 --set-frac 0.2 --set-time-steps 50 --set-k 1
+
+pdm run scripts/compare_hyperparameter.py --env h1-hurdle-v0 --optimize theta --num-cpu 4 --max-processes 2
+pdm run scripts/compare_hyperparameter.py --env h1-hurdle-v0 --optimize frac --num-cpu 4 --max-processes 2 \
+    --set-theta 0.7
+pdm run scripts/compare_hyperparameter.py --env h1-hurdle-v0 --optimize time_steps --num-cpu 8 --max-processes 1 \
+    --set-theta 0.7 --set-frac 0.2
+pdm run scripts/compare_hyperparameter.py --env h1-hurdle-v0 --optimize K --num-cpu 8 --max-processes 1 \
+    --set-theta 0.7 --set-frac 0.2 --set-time-steps 50
+pdm run scripts/compare_hyperparameter.py --env h1-hurdle-v0 --optimize seed --num-cpu 4 --max-processes 2 \
+    --set-theta 0.7 --set-frac 0.2 --set-time-steps 50 --set-k 1
